8d75296fe8b4bb1e1d31f56e7c059c9c53911bee
Jeffrey Ng <jeffreymng@netzero.net>
2016-07-13 01:53:55 -0400

    added examples for presentation

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,15 @@ Rails.application.routes.draw do
 
   # resource :article
 
+  # get 'exit', to: 'sessions#destroy', as: :logout
+
+  # get 'bungalo/:id', to: 'articles#show', as: :article
+
+  # get '/articles', to: redirect('/articles/1')
+
   resources :articles do
     resources :comments
   end
+
+
 end
